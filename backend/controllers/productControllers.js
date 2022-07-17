f3471f3b3b122366736c0cf0f17ee638b43ae4de
Gaurav <gk44035@gmail.com>
2022-07-17 22:29:04 +0530

    working_will_connect_with_database

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,3 +1,3 @@
-exports.getAllProducts=(req,res=>{
-    res.status(200).json({message:"Routen is working "})
-})
+exports.getAllProducts=(req,res)=>{
+    res.status(200).json({message:"Router is working "})
+}
---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const app=require("./app");
+
+const dotenv=require("dotenv");
+
+//config
+dotenv.config({path:"backend/config/config.env"});
+
+//create a server
+app.listen(process.env.PORT,()=>
+{
+    console.log("Server is working on http://localhost:${process.env.PORT}")
+
+})
