46dbfe5068f969be67f87d917316169579e090b0
Gaurav <gk44035@gmail.com>
2022-07-18 19:28:52 +0530

    update_product

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,8 @@
 const express=require("express");
-const { getAllProducts,createProduct } = require("../controllers/productControllers");
+const { getAllProducts,createProduct, updateProduct } = require("../controllers/productControllers");
 const router=express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/products/new").post(createProduct);
-
+router.route("/product/:id").put(updateProduct);
 module.exports=router
