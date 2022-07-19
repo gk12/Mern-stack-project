909abf19470883bb2495b781ef3a977d9bb0e4a9
Gaurav <gk44035@gmail.com>
2022-07-19 11:47:17 +0530

    app_js_middleware

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const app=express();
+const errorMiddleware=require("./middleware/error");
 
 // This method is called as a middleware in an application using this code:-
 app.use(express.json())
@@ -7,5 +8,8 @@ app.use(express.json())
 //Route imports
 const product=require("./routes/productRoute");
 app.use("/api/v1",product);
+
+//midleware for error
+app.use(errorMiddleware);
 
 module.exports=app;
