59410a30211cb684e6a3764bbf0497a91b66f17a
Gaurav <gk44035@gmail.com>
2022-07-21 17:16:42 +0530

    filter_price_and_rating

--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -5,6 +5,7 @@ class ApiFeatures{
         this.queryStr=queryStr;
     }
 
+//searching with the help of keyword
     search(){
         const keyword=this.queryStr.keyword?
         {
@@ -17,5 +18,28 @@ class ApiFeatures{
         this.query=this.query.find({...keyword});
         return this;
     }
+
+
+    // this is for category
+    filter(){
+        const queryCopy={...this.queryStr};
+
+        //removing some fields for category
+
+        const removedFields=["keyword","page","limit"];
+
+        removedFields.forEach((key)=> delete queryCopy[key]);
+ 
+        //filter price and rating
+        // console.log(queryCopy);
+        let queryStr=JSON.stringify(queryCopy);
+         queryStr=queryStr.replace(/\b(gt|gte|lt|lte)\b/g,(key)=>`$${key}`);
+
+        this.query=this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+
+
+    
 }
 module.exports=ApiFeatures;
