64e5979a31ad31f0d635b350a13b2501f2fd577a
Gaurav <gk44035@gmail.com>
2022-07-21 17:43:36 +0530

    pagination_apifeatures_js

--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -39,6 +39,15 @@ class ApiFeatures{
         return this;
     }
 
+    //content on page and we call page limit
+
+    pagination(resultPerPage)
+    {
+        const currentPage=Number(this.queryStr.page)|| 1;
+        const skip=resultPerPage*(currentPage-1);
+        this.query=this.query.limit(resultPerPage).skip(skip);
+        return this;
+    }
 
     
 }
